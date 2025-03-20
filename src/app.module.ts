3bdd742337037daf2059f1f480af387bba527d0a
izerekerie <izereuwonkundakelia@gmail.com>
2025-03-20 15:12:34 +0200

    feat: basic crud operation models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ToursModule } from './tours/tours.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ToursModule, BookingsModule, PaymentsModule, ReviewsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
